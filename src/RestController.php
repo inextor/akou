c0404fba533b58212cf188259c1c6e9a3c92eb13
Nextor Leon <nextor@gmail.com>
2019-06-06 16:46:16 -0700

    ading functionality to resteController

--- src/RestController.php
+++ src/RestController.php
@@ -6,14 +6,13 @@ class RestController
 {
 	function __construct()
 	{
+		$response = "";
 	}
 
 	function setAllowHeader()
 	{
 		$all_methods = ['POST','GET','PUT','OPTIONS','HEADER','PATCH','DELETE'];
 		$methods = Array();
-
-
 		header('Allow: '.join($methods,","));
 	}
 
@@ -21,7 +20,25 @@ class RestController
 	{
 		$method = strtolower( $_SERVER['REQUEST_METHOD'] );
 
-		if( is_callable( $this->{ $method } ) )
+		if( $method == 'HEADER' || $method == 'GET' )
+		{
+			if( isset( $this->get ) )
+			{
+				$this->get();
+				header('Content-Length: '.strlen($this->response) );
+
+				if( $method == 'GET' )
+				{
+					echo $this->response;
+				}
+			}
+			else
+			{
+				$this->sendStatus(404);
+				header('Content-length: 0');
+			}
+		}
+		else if( is_callable( $this->{ $method } ) )
 		{
 			$this->{$method}();
 		}
@@ -44,5 +61,25 @@ class RestController
 		{
 			return json_decode( file_get_contents("php://input") );
 		}
+	}
+
+	function sendStatus($code )
+	{
+		if( $code == 405 )
+		{
+			$this->setAllowHeader();
+		}
+
+		http_response_code( $code );
+		return $this;
+	}
+
+	function json($value)
+	{
+		header( 'Content-type: application/json');
+		$str = json_encode( $value );
+		$size = strlen( $str );
+		header( 'Content-Length: '.$size );
+		$this->response = $str;
 	}
 }
